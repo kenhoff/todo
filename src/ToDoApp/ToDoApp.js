e132b50a97e7b5b2ef2dab3522cdcc23890d2618
radiatorboy <scott.recore@gmail.com>
2020-06-02 13:20:40 -0600

    ran create-react-app and did some initial code cleanup

---
+++ src/ToDoApp/ToDoApp.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './ToDoApp.css';
+
+function ToDoApp() {
+  return (
+    <header>
+      
+    </header>
+    <main>
+    </main>
+    <footer>
+
+    </footer>
+  );
+}
+
+export default ToDoApp;
